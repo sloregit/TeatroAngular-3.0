0cb0d4dfcdd8cfb7182cf80f213a60f4aa8e8c95
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-29 23:37:16 +0200

    2 files modified, 3 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component, VERSION } from '@angular/core';
+import { TeatroDBService } from './teatro-db.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  datiIn;
+  constructor(private TeatroDBService: TeatroDBService) {}
+  getDati() {
+    this.TeatroDBService.getPrenotazioni$().subscribe({
+      next: (res: string) => {
+        this.datiIn = JSON.parse(res);
+        console.log(this.datiIn)
+        /*for (let elem in this.datiIn) {
+          this.arrayNomeSpettacoli.push(this.datiIn[elem].nomeSpettacolo);
+        }*/
+      },
+      error: (err) =>
+        console.error('Observer got an error: ' + JSON.stringify(err)),
+    });
+  }
 }
