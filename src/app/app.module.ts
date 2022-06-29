fbdfeb447ce157aea3fba5964ab20124ccc929c6
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-29 23:41:36 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 //
 import { AppComponent } from './app.component';
 import { TeatroDBService } from './teatro-db.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, ReactiveFormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   providers: [TeatroDBService],
