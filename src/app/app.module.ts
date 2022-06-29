96115ffcdf42cc42e87173eba2fcd2472efd9485
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-29 23:26:30 +0200

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HelloComponent } from './hello.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
